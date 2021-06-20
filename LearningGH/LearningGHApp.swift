7d1efe0a74175a097e38a76a31cbba4f9eeb593e
Adityaa Mehra <adirakash@gmail.com>
2021-06-20 13:49:38 +0530

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Adityaa Mehra on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
